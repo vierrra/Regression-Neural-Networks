b3dfc22ae8ee199b9d2b86a993aa1ffc46e833f1
Renato Vieira <vierrra@hotmail.com>
2020-05-28 01:36:10 -0300

    Indentation

--- main.py
+++ main.py
@@ -5,37 +5,37 @@ from sklearn.compose import ColumnTransformer
 from keras.models import Sequential
 from keras.layers import Dense
 
-#Buscando dados
+#Data search
 csv = pd.read_csv('dados.csv', sep=';')
 
-#Tratando dados
+#Data treatment
 le = LabelEncoder()
 csv['Tipo'] = le.fit_transform(csv['Tipo'])
 data = csv.values
 
-#Separando Atributos e Classificadores
+#Separating attributes and classifiers
 attributes     = data[:,:5]
 numberComments = data[:, 5]
 likes          = data[:, 6]
 shared         = data[:, 7]
 
-#Ajuste de atributos de classificação nao binarios
-ct = ColumnTransformer([('binario', OneHotEncoder(), [0, 1])], remainder='passthrough')
+#Adjustment of non-binary classification attributes
+ct         = ColumnTransformer([('binario', OneHotEncoder(), [0, 1])], remainder='passthrough')
 attributes = ct.fit_transform(attributes).toarray()
 
-#Modelo comentarios
+#Model comments
 modelComments = Sequential()
 modelComments.add(Dense(units=4, activation='relu'))
 modelComments.add(Dense(units=1, activation='linear'))
 modelComments.compile(optimizer = 'adam', loss = 'mean_absolute_error', metrics = ['mean_absolute_error'])
 
-#Modelo likes
+#Model likes
 modelLikes = Sequential()
 modelLikes.add(Dense(units=4, activation='relu'))
 modelLikes.add(Dense(units=1, activation='linear'))
 modelLikes.compile(optimizer = 'adam', loss = 'mean_absolute_error', metrics = ['mean_absolute_error'])
 
-#Modelo compartilhamentos
+#Model Shared
 modelShared = Sequential()
 modelShared.add(Dense(units=4, activation='relu'))
 modelShared.add(Dense(units=1, activation='linear'))
@@ -45,7 +45,6 @@ modelComments.fit(attributes, numberComments, batch_size=30, epochs=1000)
 modelLikes.fit(attributes, likes, batch_size=30, epochs=1000)
 modelShared.fit(attributes, shared, batch_size=30, epochs=1000)
 
-
 #Inputs
 postType = int(input('Informe o número de tipo da postagem: Foto[0] | Link[1] | Status[2] | Video[3]: '))
 month    = int(input('Mês: '))
@@ -53,7 +52,7 @@ day      = int(input('Dia da semana: D[1] | S[2] | T[3] | Q[4] | Q[5] | S[6] | S
 hour     = int(input('Hora: '))
 pay      = int(input('Pago: SIM[1] | NÃO[0]: '))
 
-#Predizendo comentarios
+#Predicting comments
 valuesComments = np.array([
     [postType, month, day, hour, pay]
 ])
@@ -61,7 +60,7 @@ valuesComments = ct.transform(valuesComments).toarray()
 
 averageComments = modelComments.predict(valuesComments)
 
-#Predizendo Likes
+#Predicting Likes
 valuesLikes = np.array([
     [postType, month, day, hour, pay]
 ])
@@ -69,7 +68,7 @@ valuesLikes = ct.transform(valuesLikes).toarray()
 
 averageLikes = modelLikes.predict(valuesLikes)
 
-#Predizendo compartilhamentos
+#Predicting Shared
 valuesShared = np.array([
     [postType, month, day, hour, pay]
 ])
@@ -77,7 +76,7 @@ valuesShared = ct.transform(valuesShared).toarray()
 
 averageShared = modelShared.predict(valuesShared)
 
-#Exibindo medias
+#Showing Averages
 print('Média Comentários: ', int(averageComments[0]))
 print('Média Likes: ', int(averageLikes[0]))
 print('Média Compartilhamentos: ', int(averageShared[0]))
